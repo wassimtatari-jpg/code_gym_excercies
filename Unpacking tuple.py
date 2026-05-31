7e2eb8ded075be0af7218ce5f79e79e1a1ab072e
Mohamed Wassim Tatari <wassimtatari@gmail.com>
2026-05-31 21:50:48 +0100

    Unpacking tuple
    here creat a tuple and unpacking it

---
+++ Unpacking tuple.py
@@ -0,0 +1,11 @@
+person=("wassim",38,"aleppo","python3 devoloper")
+
+name,age,city,job=person
+
+print(f"Name : {name}")
+
+print(f"Age : {age}")
+
+print(f"City : {city}")
+
+print(f"Job : {job}")
