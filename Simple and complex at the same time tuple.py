c7b0ccebbc0b79f46eb31d202b5f68e4b14b99b8
Mohamed Wassim Tatari <wassimtatari@gmail.com>
2026-05-31 21:32:50 +0100

    creat tuple

---
+++ Simple and complex at the same time tuple.py
@@ -0,0 +1,19 @@
+tuple_0=()
+
+tuple_1=(1,)
+
+tuple_5=(1,2,3,4,5)
+
+tuple_100=tuple(range(100))
+
+tuple_1000=tuple(range(1000))
+
+print("Tuple 0 elements : ",tuple_0)
+
+print("Tuple 1 elements : ",tuple_1)
+
+print("Tuple 5 elements : ",tuple_5)
+
+print("Tuple 100 elements : ",tuple_100)
+
+print("Tuple 1000 elements : ",tuple_1000)
