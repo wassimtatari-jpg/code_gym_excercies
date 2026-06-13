f0fc6befefc24888b3e9d03972d4e7dbd645e3a6
Mohamed Wassim Tatari <wassimtatari@gmail.com>
2026-06-13 06:56:14 +0100

    Fruit Bowl

---
+++ Fruit Bowl.py
@@ -0,0 +1,7 @@
+fruit_name={"banana","cherry","orange","lemmon","apple"}
+
+removed_fruit=input("Enter fruit name that you want to delet : ")
+
+fruit_name.discard(removed_fruit)
+
+print("update fruits name : ",fruit_name)
