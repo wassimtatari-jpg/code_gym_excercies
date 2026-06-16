2b806625bb3ee408fad2d051337684da03ab386f
Mohamed Wassim Tatari <wassimtatari@gmail.com>
2026-06-16 07:33:10 +0100

    Add files via upload

---
+++ Multiline Strings.py
@@ -0,0 +1,11 @@
+string_single_quote='here i use single quote'
+
+string_double_quote="here i use double quotes"
+
+string_mulitiline="""wassim study python3
+he studys every day
+he adds his small project in github"""
+
+print(string_single_quote)
+print(string_double_quote)
+print(string_mulitiline)
