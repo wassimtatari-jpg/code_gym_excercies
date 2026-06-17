87dcb2dea5d2ea2bf1a045ce98b65dfe17a2e22f
Mohamed Wassim Tatari <wassimtatari@gmail.com>
2026-06-17 07:31:44 +0100

    Add files via upload

---
+++ Negative slicing.py
@@ -0,0 +1,10 @@
+user_string=input("Enter your string : ")
+
+extracts1=user_string[-3:]
+extracts2=user_string[:-1]
+
+reverses=user_string[::-1]
+
+print(extracts1)
+print(extracts2)
+print(reverses)
