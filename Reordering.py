c7284a84a8e68a8d3254e5e0941c9f5d17dd6044
Mohamed Wassim Tatari <wassimtatari@gmail.com>
2026-06-17 08:34:31 +0100

    Reordering.

---
+++ Reordering.py
@@ -0,0 +1,10 @@
+user_input=input("Enter several words seperated by commas : ")
+
+words_list=user_input.split()
+
+print("words list is ",words_list)
+
+
+join= ' '.join(words_list)
+
+print("joined : ",join)
