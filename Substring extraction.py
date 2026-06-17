65164e5d5588b11803f580b54292602933dac2b2
Mohamed Wassim Tatari <wassimtatari@gmail.com>
2026-06-17 07:19:34 +0100

    Add files via upload

---
+++ Substring extraction.py
@@ -0,0 +1,9 @@
+user_input=input("Enter your string : ")
+
+substring1=user_input[3:9]
+
+substring2=user_input[5:]
+
+print("The substring from 3th to 8th : ",substring1)
+print("The substring from 5th to end  : ",substring2)
+
