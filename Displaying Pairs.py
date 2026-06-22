c9d8175183915366d27c7a07cfd111eebe555672
Mohamed Wassim Tatari <wassimtatari@gmail.com>
2026-06-22 07:35:37 +0100

    Add files via upload

---
+++ Displaying Pairs.py
@@ -0,0 +1,12 @@
+proudect_information={"name":"choula perfoum",
+                      "price":150,
+                      "quantinty":15
+                      }
+
+for key,value in proudect_information.items():
+    print(f"{key} : {value}")
+proudect_information["date"]=2026
+
+print("="*25)
+for key,value in proudect_information.items():
+    print(f"{key} : {value}")    
