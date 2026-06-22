1d301bbe038159ba9e0adf3b7e2ec7af4971dabf
Mohamed Wassim Tatari <wassimtatari@gmail.com>
2026-06-22 07:14:53 +0100

    Add files via upload

---
+++ Looking for keys.py
@@ -0,0 +1,10 @@
+book_information={"title":"your experince","auther":"wassim","year of puplication":2025}
+
+print(book_information)
+
+keys=book_information.keys()
+
+print(keys)
+
+for key in book_information.keys():
+    print(key)
