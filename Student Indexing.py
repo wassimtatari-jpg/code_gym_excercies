e21bbeb6e34b0fd4a69dea18e781a16fd8eeaa0d
Mohamed Wassim Tatari <wassimtatari@gmail.com>
2026-06-24 22:28:07 +0100

    Add files via upload

---
+++ Student Indexing.py
@@ -0,0 +1,7 @@
+student={
+    "name":"wassim",
+    "age":38,
+    "university":"codegym"
+}
+for index,(key,value) in enumerate(student.items(),start=1):
+    print(f"N: {index} Key: {key},Value : {value}")
