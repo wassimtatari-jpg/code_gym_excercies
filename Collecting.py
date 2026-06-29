c75578ccd574bac101647209bc38d63595397f22
Mohamed Wassim Tatari <wassimtatari@gmail.com>
2026-06-29 09:44:37 +0100

    Add files via upload

---
+++ Collecting.py
@@ -0,0 +1,14 @@
+list1=[1,2,3,4]
+list2=[5,6,7,8]
+combine=zip(list1,list2)
+print(" The combine two lists is : ")
+print(list(combine))
+
+print("The smallest element is : ")
+print(min(list1))
+
+print("The longest element is : ")
+print(max(list2))
+
+print("The sum of elements is : ")
+print(sum(list1))
