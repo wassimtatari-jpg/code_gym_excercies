d8c9678b23da15ad5a00dc2e8e5c173dd6982ecf
Mohamed Wassim Tatari <wassimtatari@gmail.com>
2026-06-29 10:41:22 +0100

    Add files via upload

---
+++ Sorting.py
@@ -0,0 +1,7 @@
+fruit=["apple","lemmon","cheery","orange","banana","kiwi","anans"]
+
+sort_fruit=sorted(fruit,key=lambda x : len(x))
+
+print("The list without sorted is : ",fruit)
+print("-"*100)
+print("The sorted list of fruit is : ",sort_fruit)
